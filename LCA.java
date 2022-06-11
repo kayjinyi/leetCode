7afb688ecdd3a7ca5e23e2c5b2bd5b6ee9b92f07
Yi Jin <KAYJINYI@GMAIL.COM>
2022-06-10 21:32:33 -0700

    Create LCA.java

---
+++ LCA.java
@@ -0,0 +1,35 @@
+// Medium
+// Given two nodes in a binary tree, find their lowest common ancestor.
+
+// Assumptions
+
+// There is no parent pointer for the nodes in the binary tree
+
+// The given two nodes are guaranteed to be in the binary tree
+
+// Examples
+
+//         5
+
+//       /   \
+
+//      9     12
+
+//    /  \      \
+
+//   2    3      14
+
+// The lowest common ancestor of 2 and 14 is 5
+
+// The lowest common ancestor of 2 and 9 is 9
+
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
